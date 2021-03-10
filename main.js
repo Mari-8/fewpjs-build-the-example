be8d5a3ac556fc2227a0c3a08189450b2da99dca
Mari-8 <ryanaltimari8@gmail.com>
2021-03-10 11:56:54 -0600

    commit tree a39ae5577fa83ddd45fd217ec95789d7580910aa

--- main.js
+++ main.js
@@ -4,7 +4,44 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorNode = document.getElementById('modal') 
+errorNode.classList.add('hidden')
 
+const posts = document.getElementsByClassName("media-post")
+for (const post of posts) {
+  let likeGlyph = post.querySelector('span.like-glyph')
+  likeGlyph.addEventListener("click", likePost)
+}
+
+
+function likePost(event) {
+      const li = event.target
+      mimicServerCall()
+      .then(res => handleLike(res)) 
+      .catch(error => handleError(error) )
+        
+      function handleLike(res) {
+        if (li.innerText == EMPTY_HEART) {
+          console.log("LIKED")
+          li.innerText = FULL_HEART
+          li.classList.add('activated-heart')
+        } else {
+          console.log("unliked") 
+          li.innerText = EMPTY_HEART 
+          li.classList.remove('activated-heart')
+        }
+      }
+      function handleError(error) {
+        let errorMessage = document.createElement('p')
+        errorMessage.innerHTML = `${error}`
+        errorNode.appendChild(errorMessage)
+        errorNode.classList.remove('hidden')
+
+        setTimeout(function() {errorNode.classList.add('hidden')}, 5000)
+        setTimeout(function() {errorMessage.innerHTML = " "}, 5000)
+        
+      }
+    }
 
 
 //------------------------------------------------------------------------------
